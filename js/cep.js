182428a2b7db9858ba4a4e454969f301c9d0707b
rickdouglas <rickmiranda23@gmail.com>
2018-05-03 20:32:56 -0300

    Coloque a funcao autocomplete por cidade no arquivo cep.js

--- js/cep.js
+++ js/cep.js
@@ -25,6 +25,33 @@ $( function() {
     });
 
 });
+
+$( function() {
+    $("#cidade").autocomplete({
+      source: function( request, response ) {
+        $.ajax( {
+          url: "app/endereco.php",
+          dataType: "json",
+          data: {
+            _action: 'get_endereco', // Método remoto
+            term: request.term // Parâmetro enviado ao método
+          },
+          success: function( data ) {
+            response( data );
+          }
+        } );
+      },
+      minLength: 2,
+      select: function( event, ui ) {
+        // Alimenta os campos a partir do retorno do método remoto
+        $("#logradouro").val(ui.item.logradouro);
+        $("#cidade").val(ui.item.cidade);
+        $("#bairro").val(ui.item.bairro);
+      }
+    });
+
+});
+
 $(document).ready(function () {
 
   var $seuCampoCpf = $("#cpf");
